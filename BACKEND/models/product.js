2712afd615b2903140759c429f0ab44f2725884b
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2021-09-19 09:12:01 +0530

    first commit
    first commit

---
+++ BACKEND/models/product.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: 'String',
+        required: true,
+    },
+    descriptions: {
+        type: String,
+        required: true,
+    },
+    richDescription: {
+        type: String,
+        default: ''
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{
+            type: String,
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Category'
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+exports.Product = mongoose.model('Product', productSchema);

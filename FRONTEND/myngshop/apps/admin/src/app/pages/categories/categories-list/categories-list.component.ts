2d81a10d1c8989cfdd7d2c2d976ad32fda611c2f
Vishal Chaware <30528484+vishalchaware@users.noreply.github.com>
2021-09-28 16:25:10 +0530

    update getCategories subscribe

--- FRONTEND/myngshop/apps/admin/src/app/pages/categories/categories-list/categories-list.component.ts
+++ FRONTEND/myngshop/apps/admin/src/app/pages/categories/categories-list/categories-list.component.ts
@@ -1,58 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { CategoriesService, Category } from '@myngshop/products';
 import { MessageService, ConfirmationService } from 'primeng/api';
-import { timer } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: 'admin-categories-list',
-  templateUrl: './categories-list.component.html'
+	selector: 'admin-categories-list',
+	templateUrl: './categories-list.component.html',
 })
-export class CategoriesListComponent implements OnInit {
+export class CategoriesListComponent implements OnInit, OnDestroy {
+	categories: Category[] = [];
+	endsubs$: Subject<any> = new Subject();
 
+	constructor(
+		private categoriesService: CategoriesService,
+		private messageService: MessageService,
+		private confirmationService: ConfirmationService,
+		private router: Router
+	) {}
 
-  categories: Category[] = [];
+	ngOnInit(): void {
+		this._getCategories();
+		this.endsubs$.complete();
+	}
 
-  constructor(
-    private categoriesService: CategoriesService,
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService,
-    private router: Router) { }
+	ngOnDestroy() {
+		this.endsubs$.next();
+		this.endsubs$.complete();
+	}
 
-  ngOnInit(): void {
-    this._getCategories();
-  }
+	deleteCategory(categoryId: string) {
+		this.confirmationService.confirm({
+			message: 'Do you want to Delete this Category?',
+			header: 'Delete Category',
+			icon: 'pi pi-exclamation-triangle',
+			accept: () => {
+				this.categoriesService.deleteCategory(categoryId).subscribe(
+					() => {
+						this._getCategories();
+						this.messageService.add({
+							severity: 'success',
+							summary: 'Success',
+							detail: 'Category is deleted!',
+						});
+					},
+					() => {
+						this.messageService.add({
+							severity: 'error',
+							summary: 'Error',
+							detail: 'Category is not deleted!',
+						});
+					}
+				);
+			},
+		});
+	}
 
-  deleteCategory(categoryId: string) {
-    this.confirmationService.confirm({
-      message: 'Do you want to Delete this Category?',
-      header: 'Delete Category',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {   
-        this.categoriesService.deleteCategory(categoryId).subscribe(
-          () => {
-            this._getCategories();
-            this.messageService.add({
-              severity:'success', 
-              summary:'Success', 
-              detail:'Category is deleted!'
-            });
-        },() => {
-            this.messageService.add({severity:'error', summary:'Error', detail:'Category is not deleted!'});
-          }
-        );
-      }
-    });
+	updateCategory(categoryid: string) {
+		this.router.navigateByUrl(`categories/form/${categoryid}`);
+	}
 
-  }
-
-  updateCategory(categoryid: string) {
-    this.router.navigateByUrl(`categories/form/${categoryid}`);
-  }
-
-  private _getCategories() {
-    this.categoriesService.getCategories().subscribe((cats) => {
-      this.categories = cats;
-    });
-  }
+	private _getCategories() {
+		this.categoriesService
+			.getCategories()
+			.pipe(takeUntil(this.endsubs$))
+			.subscribe((cats) => {
+				this.categories = cats;
+			});
+	}
 }
